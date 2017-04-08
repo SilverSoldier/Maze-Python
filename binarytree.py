5d30580b271280e46b52227d90106d02c9a0e3e7
Kavya <gkavya10@gmail.com>
2017-04-08 17:06:55 +0530

    added binary tree method of generating maze

---
+++ binarytree.py
@@ -0,0 +1,37 @@
+import random
+
+class Cell:
+    def __init__(self):
+        self.top = True
+        self.bottom = True
+        self.left = True
+        self.right = True
+
+def init_variables(rows, cols):
+    cells = [[ Cell()
+        for y in range(cols) ]
+        for x in range(rows) ]
+
+    return cells
+
+def generate_maze(cells, x, y, rows, cols):
+    sides = []
+    if y != cols-1:
+        sides.append('R')
+    if x != rows-1:
+        sides.append('D')
+
+    if sides == []:
+        return cells
+    side = random.choice(sides)
+
+    if side == 'R':
+        cells[x][y].right = False
+        cells[x][y+1].left = False
+
+    if side == 'D':
+        cells[x][y].bottom = False
+        cells[x+1][y].top = False
+
+    return cells
+    
